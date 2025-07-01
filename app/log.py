108fcd75928f63a15018ddaefb94acfe82516cfc
FreeDesertFox <senatorkotik@Administrator.localdomain>
2025-07-01 11:50:10 +0300

    Change log system

--- app/log.py
+++ app/log.py
@@ -7,10 +7,12 @@ import os, sys
 class StartLogging():
     def __init__(self, Mode: str = "console"):
         LoggingStartTime = datetime.datetime.now()
-        formatted_time = LoggingStartTime.strftime("%Y-%m-%d_%H-%M-%S")
+        FormatedDate = LoggingStartTime.strftime("%Y-%m-%d_%H-%M-%S")
+
+        LogPath = os.path.join(sys.path[0]) + "/../logs/"
 
         if Mode == "console":
             logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
         
         if Mode == "file":
-            logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", filename=f"{os.path.join(sys.path[0]) + "\\..\\logs\\"}{formatted_time}.log", filemode="a")
+            logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", filename=f"{LogPath}{FormatedDate}.log", filemode="a")
